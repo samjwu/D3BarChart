cb21eedb6d25d643cc70525e32dee25eeae88cde
samjwu <sjwu@ualberta.ca>
2024-01-13 18:18:46 -0700

    Parse dates

--- js/constants.js
+++ js/constants.js
@@ -1,3 +1,5 @@
 var graphWidth = 800;
 var graphHeight = 400;
 var barWidth = 4;
+
+var dataSource = 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json';
--- js/scripts.js
+++ js/scripts.js
@@ -10,4 +10,25 @@ document.addEventListener('DOMContentLoaded', function () {
         .append('div')
         .attr('id', 'tooltip')
         .style('opacity', 0);
+
+    d3.json(dataSource)
+        .then((jsonData) => {
+            var dates = jsonData.data.map(function (item) {
+                var year = item[0].substring(0, 4);
+                var month = item[0].substring(5, 7);
+                var quarter;
+
+                if (month === '01') {
+                    quarter = 'Q1';
+                } else if (month === '04') {
+                    quarter = 'Q2';
+                } else if (month === '07') {
+                    quarter = 'Q3';
+                } else if (month === '10') {
+                    quarter = 'Q4';
+                }
+
+                return year + ' ' + quarter;
+            });
+        });
 });
