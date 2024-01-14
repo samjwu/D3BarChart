9e2386edd2cc73b519391cb0c150fc4f2747b8b7
samjwu <sjwu@ualberta.ca>
2024-01-13 18:14:34 -0700

    Append svg and tooltip div

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
-    
+    var svgContainer = d3
+        .select('.graph')
+        .append('svg')
+        .attr('width', graphWidth + 100)
+        .attr('height', graphHeight + 60);
+
+    var tooltip = d3
+        .select('.graph')
+        .append('div')
+        .attr('id', 'tooltip')
+        .style('opacity', 0);
 });
